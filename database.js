b872464275a17eb297672eea96b3e3c120bd86cf
Chris <chauyuhin1998@gmail.com>
2023-03-27 14:57:20 -0600

    Final, added hover functions

---
+++ database.js
@@ -0,0 +1,31 @@
+const {MongoClient} = require('mongodb');
+
+const userName = "anonymous4586";
+const password = "n3ObW4PyywL5Q09I";
+const hostname = "cluster4586.ihxvlbq.mongodb.net";
+
+
+if (!userName) {
+  throw Error('Database not configured. Set environment variables');
+}
+
+const url = `mongodb+srv://${userName}:${password}@${hostname}`;
+
+const client = new MongoClient(url);
+const scoreCollection = client.db('simon').collection('score');
+
+function addScore(score) {
+  scoreCollection.insertOne(score);
+}
+
+function getHighScores() {
+  const query = {score: {$gt: 0}};
+  const options = {
+    sort: {score: -1},
+    limit: 10,
+  };
+  const cursor = scoreCollection.find(query, options);
+  return cursor.toArray();
+}
+
+module.exports = {addScore, getHighScores};
--- public/play.js
+++ public/play.js
@@ -119,7 +119,6 @@ class Game {
     scoreEl.textContent = score;
   }
 
-
   async buttonDance(laps = 1) {
     for (let step = 0; step < laps; step++) {
       for (const btn of this.buttons.values()) {
@@ -133,25 +132,37 @@ class Game {
     return buttons[Math.floor(Math.random() * this.buttons.size)];
   }
 
-  saveScore(score) {
+  async saveScore(score) {
     const userName = this.getPlayerName();
+    const date = new Date().toLocaleDateString();
+    const newScore = { name: userName, score: score, date: date };
+
+    try {
+      const response = await fetch('/api/score', {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(newScore),
+      });
+
+      // Store what the service gave us as the high scores
+      const scores = await response.json();
+      localStorage.setItem('scores', JSON.stringify(scores));
+    } catch {
+      // If there was an error then just track scores locally
+      this.updateScoresLocal(newScore);
+    }
+  }
+
+  updateScoresLocal(newScore) {
     let scores = [];
     const scoresText = localStorage.getItem('scores');
     if (scoresText) {
       scores = JSON.parse(scoresText);
     }
-    scores = this.updateScores(userName, score, scores);
-
-    localStorage.setItem('scores', JSON.stringify(scores));
-  }
-
-  updateScores(userName, score, scores) {
-    const date = new Date().toLocaleDateString();
-    const newScore = { name: userName, score: score, date: date };
 
     let found = false;
     for (const [i, prevScore] of scores.entries()) {
-      if (score > prevScore.score) {
+      if (newScore > prevScore.score) {
         scores.splice(i, 0, newScore);
         found = true;
         break;
@@ -166,7 +177,7 @@ class Game {
       scores.length = 10;
     }
 
-    return scores;
+    localStorage.setItem('scores', JSON.stringify(scores));
   }
 }
 
